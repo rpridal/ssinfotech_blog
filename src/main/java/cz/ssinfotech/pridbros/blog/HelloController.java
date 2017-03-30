63dd1d2357398cedfb1d7c26f6cbd6f5749752d5
Unknown <rpridal@gmail.com>
2017-03-30 14:57:38 +0200

    initial commit

---
+++ src/main/java/cz/ssinfotech/pridbros/blog/HelloController.java
@@ -0,0 +1,25 @@
+package cz.ssinfotech.pridbros.blog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import cz.ssinfotech.pridbros.blog.domain.Article;
+import cz.ssinfotech.pridbros.blog.repository.ArticleRepository;
+
+@RestController
+public class HelloController {
+
+	@Autowired
+	ArticleRepository articleRepository;
+	
+	@RequestMapping("/create")
+	public String create(@RequestParam("caption") String caption) {
+		Article article = new Article();
+		article.setCaption(caption);
+		articleRepository.save(article);
+		return "Article was saved;";
+	}
+
+}
---
+++ src/main/java/cz/ssinfotech/pridbros/blog/domain/Article.java
@@ -0,0 +1,44 @@
+package cz.ssinfotech.pridbros.blog.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Article {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String caption;
+	private String body;
+	@ManyToOne
+	private Author autor;
+	
+	public String getCaption() {
+		return caption;
+	}
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public Author getAutor() {
+		return autor;
+	}
+	public void setAutor(Author autor) {
+		this.autor = autor;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
